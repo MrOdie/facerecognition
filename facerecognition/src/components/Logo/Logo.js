b8f57fe5a5a20278e372bf66dbfb210f9dc53e8a
mrodie <sodonnell5214@gmail.com>
2018-11-01 13:51:02 -0400

    Secong Iteration

---
+++ facerecognition/src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import logo from './logo.png';
+import './Logo.css';
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+		 		<div className="Tilt-inner"><img style={{borderRadius: '5px'}} alt='logo' src={logo} /></div>
+			</Tilt>
+		</div>
+	)
+}
+
+export default Logo;
