7fc6534a899e15e0fc0f7bf29a78b54233163a17
mrodie <sodonnell5214@gmail.com>
2018-11-02 13:11:36 -0400

    Third Iteration

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Logo />
-        <ImageLinkForm />
-        {
-          
-          // <FaceRecognition />
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          value_area: 800
         }
-      </div>
-    );
+      }
+    }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <Particles className="particles"
+            params={particlesOptions}
+          />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit} 
+          />
+          {
+        
+            // <FaceRecognition />
+          }
+        </div>
+      );
   }
 }
 
